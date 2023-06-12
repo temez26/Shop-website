0903b8836c492d39122849c005e6df003fb61db5
Teemu Kalmari <teemu.kalmari@student.lab.fi>
2023-06-12 17:59:38 +0300

    made darkmode not to flash when brwosing on site

--- garden/wwwroot/js/site.js
+++ garden/wwwroot/js/site.js
@@ -2,21 +2,20 @@
     var body = document.body;
     var darkModeToggle = document.getElementById("darkModeToggle");
     var darkModeIcon = document.getElementById("darkModeIcon");
-   
 
     if (darkModeToggle.checked) {
         // Enable dark mode
         body.classList.add("dark-mode");
         darkModeIcon.classList.remove("fa-sun");
         darkModeIcon.classList.add("fa-moon");
-        
+
         saveModePreference("dark-mode");
     } else {
         // Enable light mode
         body.classList.remove("dark-mode");
         darkModeIcon.classList.remove("fa-moon");
         darkModeIcon.classList.add("fa-sun");
-        
+
         saveModePreference("light-mode");
     }
 }
@@ -30,7 +29,6 @@ function applySavedMode() {
     var body = document.body;
     var darkModeToggle = document.getElementById("darkModeToggle");
     var darkModeIcon = document.getElementById("darkModeIcon");
-    
 
     if (savedMode === "dark-mode") {
         // Enable dark mode
@@ -38,20 +36,22 @@ function applySavedMode() {
         darkModeToggle.checked = true;
         darkModeIcon.classList.remove("fa-sun");
         darkModeIcon.classList.add("fa-moon");
-        
     } else {
         // Enable light mode (default)
         body.classList.remove("dark-mode");
         darkModeToggle.checked = false;
         darkModeIcon.classList.remove("fa-moon");
         darkModeIcon.classList.add("fa-sun");
-        
     }
 
     // Show the content after the preferred mode is applied
     var contentWrapper = document.getElementById("contentWrapper");
     contentWrapper.classList.remove("content-hidden");
 }
+
+// Apply the saved mode on page load
+applySavedMode();
+
 
 
 var headlines = [
