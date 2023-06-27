eb30d7f2f9b097e7d786599eb81becd97b83c195
Teemu Kalmari <teemu.kalmari@student.lab.fi>
2023-06-27 16:32:35 +0300

    added products to the products website and added shopping cart not ready yet

---
+++ garden/Pages/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace garden.Pages
+{
+    public class CartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
